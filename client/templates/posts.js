5b9ccbacc0d48caa99f12a9fc700caa3b40ed11c
Blair Nilsson <blair.nilsson@gmail.com>
2015-10-08 21:38:06 +1300

    add voting

--- client/templates/posts.js
+++ client/templates/posts.js
@@ -11,10 +11,22 @@ Template.posts.events({
       Posts.insert({
         text: text,
         createdAt: new Date(), // current time
-        by:Meteor.userId() // current user
+        by:Meteor.userId(), // current user
+        up:0,
+        down:0
       });
       event.target.text.value = ""; // clear the text field
-    }
+    },
+  "click .up": function (event) {
+    Posts.update(this._id, {
+      $inc: {"up": 1}
+    });
+  },
+  "click .down": function (event) {
+    Posts.update(this._id, {
+      $inc: {"down": 1}
+    });
+  }
 });
 
 Template.post.helpers({
